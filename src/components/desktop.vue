3f1b43913d0f7cd1ae352f5e500231ad8b86345d
Adrien Maranville <Adrien@Maranville.io>
2020-06-18 19:38:44 -0600

    Adds ability to get desktop background from Unsplash

--- src/components/desktop.vue
+++ src/components/desktop.vue
@@ -1,20 +1,47 @@
 <template>
   <div>
-    <h2>Desktop</h2>
-    <template v-if="user.loggedIn">
-      <div class="alert alert-success" role="alert">
-        You are logged in
-      </div>
-    </template>
+    <div id="background-container"
+         :style="`background-image:url(${backgroundImage})`"
+         class="h-screen w-full bg-no-repeat bg-cover bg-center"
+         @contextmenu.prevent="$refs.menu.open">
+    </div>
+    <vue-context ref="menu">
+      <li>
+        <a href="#" @click.prevent="getRandomBackground">Change Background</a>
+      </li>
+    </vue-context>
   </div>
 </template>
 
-<style></style>
+<style lang="postcss" scoped>
+</style>
 
 <script>
   import { mapGetters } from 'vuex';
+  import axios from 'axios';
+  import { VueContext } from 'vue-context';
 
   export default {
+    components: {
+      VueContext,
+    },
+    data() {
+      return {
+        backgroundImage: '',
+      };
+    },
+    methods: {
+      getRandomBackground() {
+        axios
+          .get(`https://api.unsplash.com/photos/random?client_id=${process.env.VUE_APP_UNSPLASH_ACCESS_KEY}`)
+          .then((result) => {
+            this.backgroundImage = `${result.data.urls.raw}&w=1500&dpr=2`;
+          });
+      },
+    },
+    created() {
+      this.getRandomBackground();
+    },
     computed: {
       ...mapGetters({
         user: 'user',
